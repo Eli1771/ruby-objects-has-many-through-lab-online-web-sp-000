aed6a3bbaf622267385fda5e61ca73c25b0918ed
elijahmm17 <elijahmm17@gmail.com>
2019-05-27 20:02:56 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,6 +12,18 @@ class Patient
   def self.all 
     @@all 
   end 
-  
-  def 
+    
+  def new_appointment(doctor, date) 
+    appt = Appointment.new()
+  end 
+  def appointments 
+    Appointment.all.select do |a| 
+      
+    end 
+  end 
+  def doctors
+    appointments.each do |a| 
+      
+    end 
+  end 
 end 
