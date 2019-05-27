c3c4e3c7af048ead38cee75b7fdcbd80008ad2c6
elijahmm17 <elijahmm17@gmail.com>
2019-05-27 19:46:11 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,29 @@
+class Genre 
+  attr_reader :name 
+  
+  @@all = []
+  
+  def initialize(name) 
+    @name = name 
+    @@all << self
+    @songs = []
+    @artists = []
+  end 
+  
+  def self.all
+    @@all 
+  end
+  
+  def songs 
+    Song.all.select do |s| 
+      s.genre == self 
+    end 
+  end 
+  def artists 
+    results = []
+    @songs.each do |s|
+      results << s.artist 
+    end 
+    results 
+  end 
+end 
